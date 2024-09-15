892f93b636c5f5455cef802ff3b55b1722591f1d
Arjun1thakur <arjunthakur89096@gmail.com>
2024-09-15 23:51:32 +0530

    add

--- server.js
+++ server.js
@@ -7,19 +7,22 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-// Replace <username>, <password>, and <your-database-name> with your credentials and database name
 const mongoURI = 'mongodb+srv://okokokokok:z1toq6U9iiD3OSVL@cluster0.p43lk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-app.use(cors());
+app.use(cors({
+    origin:'*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+
+}));
 app.use(bodyParser.json());
-// Connect to MongoDB Atlas
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log("Connected to MongoDB Atlas"))
   .catch(err => console.error("Could not connect to MongoDB Atlas...", err));
 
-// Define a Mongoose Schema and Model for the form data
 const formDataSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
@@ -31,20 +34,16 @@ const formDataSchema = new mongoose.Schema({
 
 const FormData = mongoose.model('FormData', formDataSchema);
 
-// Define an API endpoint to handle the POST request
 app.post('/api/form', async (req, res) => {
     const { name, email, phone, state, city, carModel } = req.body;
 
-    // Basic validation
     if (!name || !email || !phone || !state || !city || !carModel) {
         return res.status(400).json({ message: 'All fields are required.' });
     }
 
     try {
-        // Create a new FormData document
         const newFormData = new FormData({ name, email, phone, state, city, carModel });
         
-        // Save the document to the database
         await newFormData.save();
         
         res.status(201).json({ message: 'Form data saved successfully!' });
@@ -54,7 +53,6 @@ app.post('/api/form', async (req, res) => {
     }
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
